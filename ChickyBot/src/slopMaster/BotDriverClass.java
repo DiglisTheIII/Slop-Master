d948cefff22aed589f59f88f9a9577a81c246e53
DiglisTheIII <98572861+DiglisTheIII@users.noreply.github.com>
2022-03-02 06:21:20 -0600

    Finally pulls token from file on PC, so no one can steal

--- ChickyBot/src/slopMaster/BotDriverClass.java
+++ ChickyBot/src/slopMaster/BotDriverClass.java
@@ -1,24 +1,20 @@
 package slopMaster;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.security.auth.login.LoginException;
-
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
-import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class BotDriverClass {
 	
 	public static void main(String[] args) throws LoginException, IOException {
-		Token token = new Token();
+		String token = Files.readAllLines(Paths.get("C:/Users/mmmmm/Desktop/botgifs/token.txt")).get(0);
 		JoeFileCount file = new JoeFileCount();
-		JDABuilder jda = JDABuilder.createDefault(token.Token);
+		JDABuilder jda = JDABuilder.createDefault(token);
 		jda.setActivity(Activity.playing("slopping innocent people (i hate them)"));
 		jda.setStatus(OnlineStatus.ONLINE);
 		jda.addEventListeners(new Commands());
